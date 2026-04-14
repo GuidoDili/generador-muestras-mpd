eba9ff2534576d52c913fceea6c07ab1ccfdf724
GuidoDili <83624162+GuidoDili@users.noreply.github.com>
2026-04-14 10:37:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -119,5 +119,15 @@ if N:
         st.markdown("---")
         st.subheader("Listado de casos seleccionados")
 
+        st.markdown("---")
+
+        st.markdown(
+            "<div style='font-size:13px; color:gray;'>"
+            "<b>Importante:</b> en ningún momento las bases de datos se cargan en la aplicación. "
+            "La herramienta únicamente genera la selección muestral a partir del tamaño de la población ingresado."
+            "</div>",
+            unsafe_allow_html=True
+        )
+
         muestra_txt = ", ".join(str(i) for i in muestra)
         st.text_area("", muestra_txt, height=100)
